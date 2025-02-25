8c48c07eb75ffa1a8120c6e6c12681f37f2b76e0
DEB-10 <guhadebasish231@gmail.com>
2025-02-25 21:08:37 +0530

    Update Con.java

--- Con.java
+++ Con.java
@@ -1,17 +1,1 @@
-package bank.management.system;
 
-import java.sql.*;
-
-public class Con {
-    Connection connection;
-    Statement statement;
-    public Con(){
-        try{
-            connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","Debasish@2004");
-            statement = connection.createStatement();
-
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-}
