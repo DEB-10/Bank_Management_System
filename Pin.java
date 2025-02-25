27b81348b8c01f33e068fbcfb381bf2aaff5c1cf
DEB-10 <guhadebasish231@gmail.com>
2025-02-25 20:51:59 +0530

    Update Pin.java

--- Pin.java
+++ Pin.java
@@ -11,7 +11,7 @@ public class Pin extends JFrame implements ActionListener {
     String pin;
     Pin(String pin){
         this.pin = pin;
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icon/atm2.png"));
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("atm2.png"));
         Image i2= i1.getImage().getScaledInstance(1550,830,Image.SCALE_DEFAULT);
         ImageIcon i3=new ImageIcon(i2);
         JLabel l3=new JLabel(i3);
